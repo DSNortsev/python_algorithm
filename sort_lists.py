f50685d4dd02c0c37424dbc7dca1c9bae735672b
Dmitry Nortsev <dmitry.nortsev@gmail.com>
2019-07-20 02:28:44 +0100

    Sort array by using insert sort

---
+++ sort_lists.py
@@ -0,0 +1,33 @@
+def insert_sort(a):
+    ''' Sort list by using insert sort '''
+    n = len(a)
+    for top in range(1, n):
+        k = top
+        while k > 0 and a[k-1] > a[k]:
+            a[k], a[k-1] = a[k-1], a[k]
+            k -= 1
+
+
+def test_sort(sort_algorithm):
+    print('Testing: ', sort_algorithm.__doc__)
+    print('Testcase #1: ', end='')
+    a = [4, 2, 5, 1, 3]
+    a_sorted = [1, 2, 3, 4, 5]
+    sort_algorithm(a)
+    print('Ok' if a == a_sorted else 'Fail')
+
+    print('Testcase #2: ', end='')
+    a = list(range(10, 20)) + list(range(0, 10))
+    a_sorted = list(range(20))
+    sort_algorithm(a)
+    print('Ok' if a == a_sorted else 'Fail')
+
+    print('Testcase #3: ', end='')
+    a = [4, 2, 4, 2, 1]
+    a_sorted = [1, 2, 2, 4, 4]
+    sort_algorithm(a)
+    print('Ok' if a == a_sorted else 'Fail')
+
+
+if __name__ == '__main__':
+    test_sort(insert_sort)
